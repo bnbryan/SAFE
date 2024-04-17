06b0a4b621ead989e6f8fe32258943a9a646cd3a
bryan <bryanwang64@outlook.com>
2024-04-17 08:22:55 -0400

    login-feature

---
+++ safe-server/src/main/java/team/ybj/controller/LoginController.java
@@ -0,0 +1,11 @@
+package team.ybj.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("login")
+@RestController
+public class LoginController {
+    
+}
