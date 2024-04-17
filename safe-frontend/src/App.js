3c83274efaaf7f6a9c298a1ec67f722e8c6daca2
Yuanchen Wang <yw8063@nyu.edu>
2024-04-17 01:52:46 -0400

    更新了些mapper 方法， 在res下新建config-wyc文件，规范化mapper命名

--- safe-frontend/src/App.js
+++ safe-frontend/src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js or your entry component
+import React from 'react';
+import { Layout, Menu, Button } from 'antd';
+const { Header, Content, Footer, Sider } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Layout>
+        <Header style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+          <div className="logo" />
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+            <Menu.Item key="1">nav 1</Menu.Item>
+            <Menu.Item key="2">nav 2</Menu.Item>
+            <Menu.Item key="3">nav 3</Menu.Item>
+          </Menu>
+          <div>
+            <Button type="primary" style={{ marginRight: '10px' }}>Login</Button>
+            <Button>Logout</Button>
+          </div>
+        </Header>
+        <Layout>
+          <Sider width={200} className="site-layout-background">
+            <Menu
+                mode="inline"
+                defaultSelectedKeys={['1']}
+                defaultOpenKeys={['sub1']}
+                style={{ height: '100%', borderRight: 0 }}
+            >
+              <Menu.Item key="1">Option 1</Menu.Item>
+              <Menu.Item key="2">Option 2</Menu.Item>
+              <Menu.Item key="3">Option 3</Menu.Item>
+              <Menu.Item key="4">Option 4</Menu.Item>
+            </Menu>
+          </Sider>
+          <Layout style={{ padding: '0 24px 24px' }}>
+            <Content
+                className="site-layout-background"
+                style={{
+                  padding: 24,
+                  margin: 0,
+                  minHeight: 280,
+                }}
+            >
+              Content goes here
+            </Content>
+          </Layout>
+        </Layout>
+        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+      </Layout>
   );
 }
 
