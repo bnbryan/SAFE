58b62dc6cbd75405c2ce0edfe9b89430f01f5073
bryan <bryanwang64@outlook.com>
2024-04-24 12:06:17 -0400

    check customer valid before login

--- safe-server/src/main/java/team/ybj/service/impl/LoginServiceImpl.java
+++ safe-server/src/main/java/team/ybj/service/impl/LoginServiceImpl.java
@@ -1,10 +1,13 @@
 package team.ybj.service.impl;
 
+import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+import team.ybj.exception.LoginException;
+import team.ybj.mappers.CustomerMapper;
 import team.ybj.pojo.LoginUser;
 import team.ybj.dto.ResponseResult;
 import team.ybj.pojo.YbjCustomer;
@@ -20,20 +23,22 @@ public class LoginServiceImpl implements LoginService {
 
     @Autowired
     private AuthenticationManager authenticationManager;
+    @Resource
+    private CustomerMapper customerMapper;
 
     @Override
-    public ResponseResult login(YbjCustomer customer) {
+    public ResponseResult<Map<String,String>> login(YbjCustomer customer) {
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(customer.getCemail(), customer.getCpassword());
 
         Authentication authenticate = authenticationManager.authenticate(authenticationToken);
         if (Objects.isNull(authenticate)) {
-            throw new RuntimeException("login failed");
+            throw new LoginException("Username or password is incorrect/invalid");
         }
         LoginUser loginUser = (LoginUser) authenticate.getPrincipal();
         String cemail = loginUser.getCustomer().getCemail();
         String jwt = JwtUtil.generateToken(cemail);
         Map<String, String> jwtMap = new HashMap<>();
         jwtMap.put("token", jwt);
-        return new ResponseResult(200, "login success", jwtMap);
+        return new ResponseResult<>(200, "login success", jwtMap);
     }
 }
