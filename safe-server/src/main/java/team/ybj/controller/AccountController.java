6a11fa09885c9610341c64930670c03eba4a2332
bryan <bryanwang64@outlook.com>
2024-04-24 13:32:28 -0400

    add get accounts api

--- safe-server/src/main/java/team/ybj/controller/AccountController.java
+++ safe-server/src/main/java/team/ybj/controller/AccountController.java
@@ -2,8 +2,11 @@ package team.ybj.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import team.ybj.dto.ResponseResult;
 import team.ybj.pojo.YbjAccount;
 import team.ybj.service.AccountService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("account")
@@ -17,6 +20,13 @@ public class AccountController {
         System.out.println(id);
         YbjAccount account = accountService.findAccountById(id);
         return account;
+    }
+
+    @GetMapping("/all/{cid}")
+    @ResponseBody
+    public ResponseResult<List<YbjAccount>> getAllAccount(@PathVariable("cid") Long cid) {
+        List<YbjAccount> allAccountsOfCustomer = accountService.findAllAccounts(cid);
+        return new ResponseResult<>(200, "success", allAccountsOfCustomer);
     }
 
     @GetMapping("test")
