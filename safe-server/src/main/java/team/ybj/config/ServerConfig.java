8b5385c03cc1335ddccadb4fffd9c685a7aa09d1
bryan <bryanwang64@outlook.com>
2024-04-23 21:44:46 -0400

    change http to https

---
+++ safe-server/src/main/java/team/ybj/config/ServerConfig.java
@@ -0,0 +1,44 @@
+package team.ybj.config;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class ServerConfig {
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        // 创建 Tomcat 容器工厂
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                // 安全约束配置，确保所有数据通过 HTTPS 传输
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*"); // 应用于所有路径
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+
+        // 添加额外的 HTTP 连接器，用于重定向
+        tomcat.addAdditionalTomcatConnectors(redirectConnector());
+        return tomcat;
+    }
+
+    private Connector redirectConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(8080); // HTTP 端口
+        connector.setSecure(false);
+        connector.setRedirectPort(8443); // HTTPS 端口
+        return connector;
+    }
+}
